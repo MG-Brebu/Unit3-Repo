8559c1a600e101ddbfd39a645c829690f1af61af
MGBrebu <mgbrebu7@gmail.com>
2022-10-26 18:21:28 +0100

    Added GitHub Link Button

---
+++ Unit3Challenge/Assets/Challenge 3/Scripts/GithubLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubLink : MonoBehaviour
+{
+    public void OpenLink()
+    {
+        Application.OpenURL("https://github.com/MG-Brebu/Unit3-Repo");
+    }
+}
